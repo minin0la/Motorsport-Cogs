67c20cb9bb17bd07518fe17c8ebfe5be55388266
Natawat Hebnak <minin0la17@gmail.com>
2020-03-19 19:00:05 +0800

    print varible

--- Motorsport/Motorsport.py
+++ Motorsport/Motorsport.py
@@ -56,8 +56,8 @@ class Motorsport(Order, Database, commands.Cog):
         author = ctx.author
         guild = ctx.guild
         date = str(datetime.datetime.now().strftime("%d/%m/%y"))
+        print(await super().order(ctx, car_name=None))
         name, ordering_channel, finished_order = await super().order(ctx, car_name=None)
-        print(name, ordering_channel, finished_order)
         if finished_order is False:
             await ordering_channel.delete()
             return
