b4d230378c093ccfd15b867b90c65243190c7d78
Natawat Hebnak <minin0la17@gmail.com>
2020-04-23 02:36:12 +0800

    Added Checking for stock_price None

--- Motorsport/Motorsport.py
+++ Motorsport/Motorsport.py
@@ -70,7 +70,7 @@ class Motorsport(Order, Database, commands.Cog):
             message = "```fix\n{}```\n>>> ".format(z)
             counting = 0
             for x in veh_price:
-                if x['Price']['Stock_Price'] != ['Price']['Normal'] and x['Type'] == z:
+                if x['Price']['Stock_Price'] != ['Price']['Normal'] and x['Type'] == z and x['Price']['Stock_Price'] is not None:
                     message = message + "**{}** has the wrong price set (${:,}).\nCorrect Price:```{}```".format(x['Name'], x['Price'], y['Price'].replace(',','').replace('$','').replace(' ',''))
                     detected = True
                     if counting < 10:
